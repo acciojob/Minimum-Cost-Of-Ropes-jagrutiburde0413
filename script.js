e56d86230f63c4d695eab78212af0f1754f6c13b
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-03-17 23:22:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function calculateMinCost() {
-  //your code here
-  
-  
+ var inputData=document.querySelector("#rope-lengths");
+	var inputArr=inputData.split(",")
+	for(var i=0;i<inputArr.length;i++){
+		inputArr[i]=Number(inputArr[i]);
+	}
+  var cost=0;
+	inputArr.sort(function(a,b){
+		return a-b;
+	})
+	while(inputArr.length>1){
+			var newRope=inputArr[0]+inputArr[1];
+          cost=cost+newRope;
+		inputArr.splice(0,2)
+		inputArr.push(newRope)
+		inputArr.sort(function (a,b){
+		return a-b;
+	}
+					  
+  )
+
+
+document.querySelector("#result").testContent=cost;
+		
+	}
   
 }  
