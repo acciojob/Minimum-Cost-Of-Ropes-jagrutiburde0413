9089110d837af0056de7fef1e73ab5d625fed230
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-25 00:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,82 @@
-function calculateMinCost() {
-  //your code here
-  // read data from input element;
-  var inputData = document.querySelector("#rope-lengths").value;
-  var inputArr = inputData.split(",");
-
-
-  for (var i = 0; i < inputArr.length; i++) {
-    inputArr[i] = parseInt(inputArr[i]);
+function findMinimumCostOfRopes(ropeLengths) {
+  // Convert the comma-separated string of rope lengths to an array of integers
+  const ropes = ropeLengths.split(',').map(Number);
+  
+  // Initialize a priority queue with the rope lengths as the keys
+  const pq = new PriorityQueue();
+  for (let i = 0; i < ropes.length; i++) {
+    pq.insert(ropes[i], ropes[i]);
   }
-  var cost = 0;
-	
-  inputArr.sort(function(a,b){
-	return a-b;
-	})
-  while (inputArr.length > 1) {
-	 
-    var newRope = inputArr[0] + inputArr[1];
-    cost += newRope;
-
-    // delete first two element
-    inputArr.splice(0, 2);
-    inputArr.push(newRope);
-	   inputArr.sort(function(a,b){
-	return a-b;
-	})
-    //alert(newRope);
+  
+  // Combine the ropes until there is only one left
+  let totalCost = 0;
+  while (pq.size() > 1) {
+    const min1 = pq.delMin();
+    const min2 = pq.delMin();
+    const combinedLength = min1.key + min2.key;
+    totalCost += combinedLength;
+    pq.insert(combinedLength, combinedLength);
   }
-  document.querySelector("#result").textContent = cost;
+  
+  return totalCost;
 }
+
+class PriorityQueue {
+  constructor() {
+    this.pq = [null];
+    this.n = 0;
+  }
+  
+  size() {
+    return this.n;
+  }
+  
+  less(i, j) {
+    return this.pq[i].priority < this.pq[j].priority;
+  }
+  
+  swap(i, j) {
+    [this.pq[i], this.pq[j]] = [this.pq[j], this.pq[i]];
+  }
+  
+  swim(k) {
+    while (k > 1 && this.less(k, Math.floor(k / 2))) {
+      this.swap(k, Math.floor(k / 2));
+      k = Math.floor(k / 2);
+    }
+  }
+  
+  sink(k) {
+    while (2 * k <= this.n) {
+      let j = 2 * k;
+      if (j < this.n && this.less(j + 1, j)) j++;
+      if (!this.less(j, k)) break;
+      this.swap(k, j);
+      k = j;
+    }
+  }
+  
+  insert(key, val) {
+    this.pq[++this.n] = { key, val, priority: Math.random() };
+    this.swim(this.n);
+  }
+  
+  delMin() {
+    const min = this.pq[1];
+    this.swap(1, this.n--);
+    this.sink(1);
+    this.pq[this.n + 1] = null;
+    return min;
+  }
+}
+
+const form = document.querySelector('form');
+const input = document.querySelector('#rope-lengths');
+const result = document.querySelector('#result');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const ropeLengths = input.value;
+  const minimumCost = findMinimumCostOfRopes(ropeLengths);
+  result.textContent = `Minimum cost: ${minimumCost}`;
+});
